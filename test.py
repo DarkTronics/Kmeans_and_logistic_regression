94713daa28c6582e502477a82c1b8c4d875a40bb
Xinwei Guo <xguo50@binghamton.edu>
2024-11-09 20:10:47 -0500

    kmean added

--- test.py
+++ test.py
@@ -10,10 +10,154 @@ from sklearn.metrics import precision_score, recall_score, f1_score, confusion_m
 import seaborn as sns
 from collections import Counter
 from scipy.spatial.distance import cdist
+import random as rd
 
-class Kmeans:
-    def __init__(self) -> None:
-        pass
+class myKMeans:
+    def __init__(self, n_clusters, iters):
+        """
+        KMeans Class constructor.
+  
+        Args:
+          n_clusters (int) : Number of clusters used for partitioning.
+          iters (int) : Number of iterations until the algorithm stops.
+  
+        """
+        self.n_clusters = n_clusters
+        self.iters = iters
+        
+    def kmeans_plus_plus(self, X, n_clusters):
+        """
+        My implementation of the KMeans++ initialization method for computing the centroids.
+  
+        Args:
+            X (ndarray): Dataset samples
+            n_clusters (int): Number of clusters
+  
+        Returns:
+            centroids (ndarray): Initial position of centroids
+        """
+        # Assign the first centroid to a random sample from the dataset.
+        idx = rd.randrange(len(X))
+        centroids = [X[idx]]
+  
+        # For each cluster
+        for _ in range(1, n_clusters):
+  
+            # Get the squared distance between that centroid and each sample in the dataset
+            squared_distances = np.array([min([np.inner(centroid - sample,centroid - sample) for centroid in centroids]) for sample in X])
+  
+            # Convert the distances into probabilities that a specific sample could be the center of a new centroid
+            proba = squared_distances / squared_distances.sum()
+  
+            for point, probability in enumerate(proba):
+                # The farthest point from the previous computed centroids will be assigned as the new centroid as it has the highest probability.
+                if probability == proba.max():
+                    centroid = point
+                    break
+  
+            centroids.append(X[centroid])
+  
+        return np.array(centroids)
+  
+    def find_closest_centroids(self, X, centroids):
+        """
+        Computes the distance to the centroids and assigns the new label to each sample in the dataset.
+  
+        Args:
+            X (ndarray): Dataset samples  
+            centroids (ndarray): Number of clusters
+  
+        Returns:
+            idx (ndarray): Closest centroids for each observation
+  
+        """
+  
+        # Set K as number of centroids
+        K = centroids.shape[0]
+  
+        # Initialize the labels array to 0
+        label = np.zeros(X.shape[0], dtype=int)
+  
+        # For each sample in the dataset
+        for sample in range(len(X)):
+            distance = []
+            # Take every centroid
+            for centroid in range(len(centroids)):
+                # Compute Euclidean norm between a specific sample and a centroid
+                norm = np.linalg.norm(X[sample] - centroids[centroid])
+                distance.append(norm)
+  
+            # Assign the closest centroid as it's label
+            label[sample] = distance.index(min(distance))
+  
+        return label
+  
+    def compute_centroids(self, X, idx, K):
+        """
+        Returns the new centroids by computing the mean of the data points assigned to each centroid.
+  
+        Args:
+            X (ndarray): Dataset samples 
+            idx (ndarray): Closest centroids for each observation 
+            K (int): Number of clusters
+  
+        Returns:
+            centroids (ndarray): New centroids computed
+        """
+  
+        # Number of samples and features
+        m, n = X.shape
+  
+        # Initialize centroids to 0
+        centroids = np.zeros((K, n))
+  
+        # For each centroid
+        for k in range(K):   
+            # Take all samples assigned to that specific centroid
+            points = X[idx == k]
+            # Compute their mean
+            centroids[k] = np.mean(points, axis=0)
+  
+        return centroids
+  
+    def fit_predict(self, X):
+        """
+        My implementation of the KMeans algorithm.
+  
+        Args:
+            X (ndarray): Dataset samples
+  
+        Returns:
+            centroids (ndarray):  Computed centroids
+            labels (ndarray):     Predicts for each sample in the dataset.
+        """
+        # Number of samples and features
+        m, n = X.shape
+  
+        # Compute initial position of the centroids
+        initial_centroids = self.kmeans_plus_plus(X, self.n_clusters)
+  
+        centroids = initial_centroids   
+        labels = np.zeros(m)
+        
+        prev_centroids = centroids
+  
+        # Run K-Means
+        for i in range(self.iters):
+            # For each example in X, assign it to the closest centroid
+            labels = self.find_closest_centroids(X, centroids)
+  
+            # Given the memberships, compute new centroids
+            centroids = self.compute_centroids(X, labels, self.n_clusters)
+            
+            # Check if centroids stopped changing positions
+            if centroids.tolist() == prev_centroids.tolist():
+                print(f'K-Means converged at {i+1} iterations')
+                break
+            else:
+                prev_centroids = centroids
+  
+        return labels, centroids
 
 input = input("CSV file name: ")
 data = pd.read_csv(input)
@@ -28,35 +172,45 @@ else:
 
 for i in range(30, 90, 20):
     X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=(i/100), random_state=42)
+    X2 = np.array([[1, 2],
+              [1.5, 1.8],
+              [5, 8 ],
+              [8, 8],
+              [1, 0.6],
+              [9,11],
+              [1,3],
+              [8,9],
+              [0,3],
+              [5,4],
+              [6,4],])
 
     # Step 1: Train k-means on X_train
-    kmeans = KMeans(n_clusters=2, random_state=42)
-    kmeans.fit(X_train)
-
+    kmeans = myKMeans(2, 50)
+    mykmeans_labels, mykmeans_centers = kmeans.fit_predict(X_train)
     # Step 2: Determine majority label for each cluster, cluster labels will either be [0,1] or [1,0]
-    cluster_labels = np.zeros(kmeans.n_clusters)
-    for cluster_idx in range(kmeans.n_clusters):
+    cluster_labels = np.zeros(2)
+    for cluster_idx in range(2):
         # Get indices of X_train points in this cluster
-        cluster_points_idx = np.where(kmeans.labels_ == cluster_idx)
         # Get the most common y_train label in this cluster using the indices of all the 0s or 1s where in the kmeans label
+        cluster_points_idx = np.where(mykmeans_labels == cluster_idx)
         common_label = Counter(y_train[cluster_points_idx]).most_common(1)[0][0]
         cluster_labels[cluster_idx] = common_label
 
     # Step 3: Classify X_test points
     test_labels = []
     for test_point in X_test:
-        # Compute current point's distances to each centroid and put in a list
-        distances = cdist([test_point], kmeans.cluster_centers_, 'euclidean')
-        # Get index of closest centroid from this current test point
-        closest_centroid_idx = np.argmin(distances)
+        # Compute current point's distances to each centroid and put in a list, 
+        # Get index of closest centroid from this current test point, 
         # Assign label of closest centroid to the test label, basically saying this point here is the label of this 
+        distances = cdist([test_point], mykmeans_centers, 'euclidean')
+        closest_centroid_idx = np.argmin(distances)
         test_labels.append(cluster_labels[closest_centroid_idx])
 
     accuracy = metrics.accuracy_score(y_test, test_labels)
     print("Accuracy:", accuracy)
-#     precision = precision_score(y_train, predictions)
-#     recall = recall_score(y_train, predictions)
-#     f1 = f1_score(y_train, predictions)
+#     precision = precision_score(y_train, mykmeans_labels)
+#     recall = recall_score(y_train, mykmeans_labels)
+#     f1 = f1_score(y_train, mykmeans_labels)
 
 #     print("Precision:", precision)
 #     print("Recall:", recall)
